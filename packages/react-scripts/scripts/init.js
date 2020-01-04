cf50838f5456ff9f381e2fb43b35c37368630c5d
Harshit Agrawal <harshit@smilebots.com>
2020-01-04 23:49:49 +0530

    initialising custom template

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -288,6 +288,7 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log('harshitIsGod');
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
